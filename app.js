7593fa0ae8bda78d15c2cc8fd7aa286efdcd0f11
Anara <berdali.aygerim9@gmail.com>
2024-01-08 13:44:00 +0000

    selectPasswordOptions func created; isNaN and parseInt added;  setTimeout removed

--- app.js
+++ app.js
@@ -4,25 +4,7 @@ var specialCharacters = [
   '%',
   '+',
   '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  '/'
   
 ];
 
@@ -39,24 +21,8 @@ var lowerCasedCharacters = [
   'f',
   'g',
   'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  'i'
+
 ];
 
 // Array of uppercase characters to be included in password
@@ -72,21 +38,8 @@ var upperCasedCharacters = [
   'I',
   'J',
   'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  'L'
+
 ];
 
 
@@ -114,26 +67,20 @@ function getPasswordOptions() {
   //ask user about password length
   passwordLength = parseInt(prompt("How many characters would you like your password to contain?"));
 
-  if (!(passwordLength >= 8 && passwordLength <= 128)) {
-    alert("Please choose a number between 8 and 128");
+  if (isNaN(passwordLength) || !(passwordLength >= 8 && passwordLength <= 128)) {
+    alert("Please enter a valid number between 8 and 128");
     getPasswordOptions(); // Use recursion to prompt again
   } else {
     selectPasswordOptions();
   }
   
-    
-      //what if all password character options are pressed CANCEL 
+      //if all password character options are pressed CANCEL 
   if ( !lowerCase && !upperCase && !numNumber && !symbol ) {
       alert("Sorry, password cannot be gererated. Choose at least one password option. ")
       getPasswordOptions();
       
     } 
     }
-
-     // getPasswordOptions()  ///recursive call
-  
-
-
 
 //-----------------------------Function to generate password with user input---------------------------------//
 let passwordNew;
@@ -172,14 +119,12 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  passwordText.value = '';
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
-  setTimeout(function() {
-    passwordText.value = '';
-  }, 5000); // Clear the password after 5 seconds
 }
 
 // Add event listener to generate button
